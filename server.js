b44d772fe8ddcb56eb26784794c857041f073a6a
David Hauck <dhauck@mac.com>
2020-03-12 20:31:04 -0400

    changed port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const dbConnection = require('./database')
 const MongoStore = require('connect-mongo')(session)
 const passport = require('./passport');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
